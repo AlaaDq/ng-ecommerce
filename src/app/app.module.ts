c3735fc06f3bed66785ebae7d0b6d52e3a37c5b8
AlaaDq <alaadq70@gmail.com>
2020-10-07 16:52:42 +0300

    angular10 typescript downgraded problem with file upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
-import { ManageCategoriesComponent } from './admin/components/manage-categories/manage-categories.component';
-import { ManageOrdersComponent } from './admin/components/manage-orders/manage-orders.component';
-import { ManageUsersComponent } from './admin/components/manage-users/manage-users.component';
+// import { ManageCategoriesComponent } from './admin/components/manage-categories/manage-categories.component';
+// import { ManageOrdersComponent } from './admin/components/manage-orders/manage-orders.component';
+// import { ManageUsersComponent } from './admin/components/manage-users/manage-users.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
@@ -23,14 +23,19 @@ import { AlertComponent } from './shared/alert/alert.component';
 import { ApplicationErrorComponent } from './shared/application-error/application-error.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { ResourceNotFoundComponent } from './shared/resource-not-found/resource-not-found.component';
+import { AdminModule } from './admin/admin.module';
+import { NgxModule } from './shared/ngx/ngx.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MaterialModule} from './shared/material/material.module';
+// import { FilesModule } from './shared/files/files.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    ManageCategoriesComponent,
-    ManageOrdersComponent,
-    ManageUsersComponent,
+    // ManageCategoriesComponent,
+    // ManageOrdersComponent,
+    // ManageUsersComponent,
     HomeComponent,
     LoginComponent,
     RegisterComponent,
@@ -45,12 +50,18 @@ import { ResourceNotFoundComponent } from './shared/resource-not-found/resource-
     AlertComponent,
     ApplicationErrorComponent,
     PageNotFoundComponent,
-    ResourceNotFoundComponent
+    ResourceNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AdminModule,
+    NgxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // FilesModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/ngx/ngx.module.ts
+++ src/app/shared/ngx/ngx.module.ts
@@ -1,12 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
+const ngxComponents = [
+  CarouselModule.forRoot(),
+  TooltipModule.forRoot(),
+  ModalModule.forRoot(),
+  PopoverModule.forRoot(),
+  AccordionModule.forRoot(),
+  PaginationModule.forRoot(),
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ngxComponents
+  ],
+  exports:[
+    ngxComponents
   ]
 })
-export class NgxModule { }
+export class NgxModule {
+}
